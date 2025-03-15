e4ee16bb8e0b567ea922cba6ad643928e6d9d0ca
vasup <artemcornobaj284@gmailc.om>
2025-03-15 10:14:44 +0000

    add Calculator

---
+++ calc.py
@@ -0,0 +1,22 @@
+def calculator(num1, num2, op):
+    if op == "+":
+        return num1 + num2
+    elif op == "-":
+        return num1 - num2
+    elif op == "*":
+        return num1 * num2
+    elif op == "/":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Помилка: ділення на нуль!"
+    else:
+        return "Невідома операція"
+
+# Введення чисел і операції з клавіатури
+a = float(input("Введіть перше число: "))
+b = float(input("Введіть друге число: "))
+operation = input("Введіть операцію (+, -, *, /): ")
+
+# Виведення результату
+print(calculator(a, b, operation))
